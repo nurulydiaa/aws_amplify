f9aae9cdd4fae98eb42b2b8076c29c89025c6ed4
nurulydiaa <nurulydiaa@gmail.com>
2020-12-25 23:17:31 +0800

    aws part 1 and 2

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,36 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-east-1:c0289462-c2e0-4a15-989c-d649d4f14605",
+                            "Region": "us-east-1"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "us-east-1_wFiZAsPdH",
+                        "AppClientId": "52ave4iirofq11sh060p4hp792",
+                        "AppClientSecret": "43137jukj0c9f5at54tohpuh1seg25pm6e8jeejvdrfi3d0ftk1",
+                        "Region": "us-east-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                }
+            }
+        }
+    }
+}''';
